e65c1bb6d5857cde2e52fc72bdb1a2b3287cc203
ajosan25 <97420031+ajosan25@users.noreply.github.com>
2023-09-24 15:33:21 -0700

    Update app

--- personal-website/src/App.tsx
+++ personal-website/src/App.tsx
@@ -1,14 +1,37 @@
-import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Welcome to my website</p>
+        <p>Angad Singh Josan</p>
+        <h6>Programmer, Debater, Student</h6>
+        <Projects />
       </header>
     </div>
   );
+}
+
+async function fetchGithubRepos(url:string) {
+  const resp = await fetch(url)
+  const json = await resp.json();
+
+  var text:string = ""
+  for(let i = 0; i < json.length; i++){
+    text += json[i].html_url
+  }  
+  
+  return text;
+}
+
+function Projects() {
+  var text = fetchGithubRepos("https://api.github.com/users/ajosan25/repos")
+  console.log(text)
+  return (
+    <>
+      <p></p>
+    </>
+  )
 }
 
 export default App;
